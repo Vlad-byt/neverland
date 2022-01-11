ba1b904256457fc1ab05c9669f67feb7499b9a24
PRunich <p.runich@oblakogroup.ru>
2022-01-11 15:31:56 +0300

    add abstract classed

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,58 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
+}
+
+abstract class Game {
+    Event[] events;
+
+    Game(Event[] events) {}
+
+    public void start() {}
+
+    public void end() {}
+
+    private void gameLoop() {}
+
+    private void init() {}
+}
+
+abstract class Player {
+    StatsManager stats;
+    Scanner input;
+
+    Player(Stat[] stats) {}
+}
+
+abstract class Event {
+    Stat[] statInfluence;
+    String msg;
+
+    Event(String msg, Stat[] statInfluence) {}
+
+    private void condition() {}
+    private void afterEvent() {}
+}
+
+abstract class StatsManager {
+    Stat[] stats;
+
+    StatsManager(Stat[] stats) {}
+
+    public void changeValue(Stat stat, int newValue) {}
+}
+
+abstract class Stat {
+    String name;
+    int    value;
+
+    Stat(String name, Integer initialValue) {}
+
+    public void setValue(int value) {}
 }
